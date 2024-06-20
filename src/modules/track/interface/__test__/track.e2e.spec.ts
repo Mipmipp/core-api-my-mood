a7c1a8dfb0d655c1dcf502a9458171c178b976da
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-20 20:41:46 -0300

    Update tests

--- src/modules/track/interface/__test__/track.e2e.spec.ts
+++ src/modules/track/interface/__test__/track.e2e.spec.ts
@@ -9,6 +9,7 @@ import { testModuleBootstrapper } from '@/tests/test.module.bootstrapper';
 
 import { MoodType } from '../../domain/mood.type';
 import { CreateTrackDto } from '../dto/create-track.dto';
+import { GetUserTrackDto } from '../dto/get-user-track.dto';
 import { UpdateTrackDto } from '../dto/update-track.dto';
 
 describe('Track - [/track]', () => {
@@ -85,11 +86,57 @@ describe('Track - [/track]', () => {
     });
   });
 
+  describe('Get - [GET /track]', () => {
+    it('should return a specific track', async () => {
+      const getUserTrackDto: GetUserTrackDto = {
+        userId: 1,
+        day: 1,
+        month: 1,
+        year: 2024,
+      };
+
+      return request(app.getHttpServer())
+        .get('/track')
+        .auth(userToken, { type: 'bearer' })
+        .send(getUserTrackDto)
+        .expect(HttpStatus.OK)
+        .then(({ body }) => {
+          const expectedResponse = expect.objectContaining({
+            ...getUserTrackDto,
+          });
+
+          expect(body).toEqual(expectedResponse);
+        });
+    });
+
+    it('should not be able to get a track with a foreign user id', async () => {
+      const getUserTrackDto: GetUserTrackDto = {
+        userId: 2,
+        day: 1,
+        month: 1,
+        year: 2024,
+      };
+
+      return request(app.getHttpServer())
+        .get('/track')
+        .auth(userToken, { type: 'bearer' })
+        .send(getUserTrackDto)
+        .expect(HttpStatus.FORBIDDEN)
+        .then(({ body }) => {
+          const expectedResponse = expect.objectContaining({
+            message: 'Users cannot manage foreign tracks.',
+          });
+
+          expect(body).toEqual(expectedResponse);
+        });
+    });
+  });
+
   describe('Post - [POST /track]', () => {
     const createTrackto: CreateTrackDto = {
       userId: 1,
-      day: 1,
-      month: 1,
+      day: 3,
+      month: 5,
       year: 2024,
       mood: MoodType.SAD,
       note: 'test',
@@ -104,6 +151,22 @@ describe('Track - [/track]', () => {
         .then(({ body }) => {
           const expectedResponse = expect.objectContaining({
             ...createTrackto,
+          });
+
+          expect(body).toEqual(expectedResponse);
+        });
+    });
+
+    it('should not be able to create a track if it already exists', async () => {
+      return request(app.getHttpServer())
+        .post('/track')
+        .auth(userToken, { type: 'bearer' })
+        .send(createTrackto)
+        .expect(HttpStatus.CONFLICT)
+        .then(({ body }) => {
+          const expectedResponse = expect.objectContaining({
+            message:
+              'Track already exists for user 1 on day 3, month 5 and year 2024',
           });
 
           expect(body).toEqual(expectedResponse);
