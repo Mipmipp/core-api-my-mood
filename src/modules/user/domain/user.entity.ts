3fd5540b35cf56f0b1b8db515629642ddeab9906
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-20 02:11:23 -0300

    Revert exclude from user entity

--- src/modules/user/domain/user.entity.ts
+++ src/modules/user/domain/user.entity.ts
@@ -1,11 +1,7 @@
-import { Exclude } from 'class-transformer';
-
 import { Base } from '../../../common/domain/base.domain';
 
 export class User extends Base {
   username: string;
   email: string;
-
-  @Exclude()
   password: string;
 }
