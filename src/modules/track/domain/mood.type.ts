d0e66333b2c2c37cc5929f409facbee68cde3dfb
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-20 15:07:59 -0300

    Add mood type

---
+++ src/modules/track/domain/mood.type.ts
@@ -0,0 +1,12 @@
+export enum MoodType {
+  ANGRY = 'ANGRY',
+  CALM = 'CALM',
+  CONFUSE = 'CONFUSE',
+  COOL = 'COOL',
+  CRYING = 'CRYING',
+  HAPPY = 'HAPPY',
+  LOVED = 'LOVE',
+  NERVOUS = 'NERVOUS',
+  SAD = 'SAD',
+  SURPRISE = 'SURPRISE',
+}
