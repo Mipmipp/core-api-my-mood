2f56749f5e233c048ac24fdb5589f5bb96757d50
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-20 15:06:39 -0300

    Add track service

---
+++ src/modules/track/application/service/track.service.ts
@@ -0,0 +1,45 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Track } from '../../domain/track.entity';
+import { CreateTrackDto } from '../../interface/dto/create-track.dto';
+import { UpdateTrackDto } from '../../interface/dto/update-track.dto';
+import {
+  fromCreateTrackDtoToTrack,
+  fromUpdateTrackDtoToTrack,
+} from '../mapper/track.mapper';
+import {
+  TRACK_REPOSITORY_KEY,
+  TrackRepository,
+} from '../repository/track.repository.interface';
+
+@Injectable()
+export class TrackService {
+  constructor(
+    @Inject(TRACK_REPOSITORY_KEY)
+    private readonly trackRepository: TrackRepository,
+  ) {}
+
+  async create(createTrackDto: CreateTrackDto): Promise<Track> {
+    const trackMapper = fromCreateTrackDtoToTrack(createTrackDto);
+
+    return await this.trackRepository.create(trackMapper);
+  }
+
+  async findOneById(id: number): Promise<Track> {
+    return this.trackRepository.findOneById(id);
+  }
+  async findOneByIdOrFail(id: number): Promise<Track> {
+    return this.trackRepository.findOneByIdOrFail(id);
+  }
+
+  async updateOrFail(id: number, updates: UpdateTrackDto): Promise<Track> {
+    const updatesMapped = fromUpdateTrackDtoToTrack(updates);
+
+    const trackUpdated = await this.trackRepository.updateOrFail(
+      id,
+      updatesMapped,
+    );
+
+    return trackUpdated;
+  }
+}
