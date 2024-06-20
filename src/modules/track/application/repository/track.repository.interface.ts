9b76e6a045fcac7df4d0e801b1adf076da019b85
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-20 15:06:25 -0300

    Add TrackRepositoryInterface

---
+++ src/modules/track/application/repository/track.repository.interface.ts
@@ -0,0 +1,10 @@
+import { Track } from '../../domain/track.entity';
+
+export const TRACK_REPOSITORY_KEY = 'TRACK_REPOSITORY';
+
+export interface TrackRepository {
+  create(track: Track): Promise<Track>;
+  findOneById(id: number): Promise<Track>;
+  findOneByIdOrFail(id: number): Promise<Track>;
+  updateOrFail(id: number, updates: Track): Promise<Track>;
+}
