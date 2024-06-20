2bbc17e268d23aba16902a9364c696a06c3e4352
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-19 21:07:46 -0300

    Update importations

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserMapper } from './application/mapper/user.mapper';
 import { USER_REPOSITORY_KEY } from './application/repository/user.repository.interface';
+import { UserQueryService } from './application/service/user.service';
 import { UserMysqlRepository } from './infrastructure/persistence/user.mysql.repository';
 import { UserSchema } from './infrastructure/persistence/user.schema';
 import { UserController } from './interface/user.controller';
@@ -12,8 +13,12 @@ import { UserController } from './interface/user.controller';
   controllers: [UserController],
   providers: [
     UserMapper,
+    UserQueryService,
     { provide: USER_REPOSITORY_KEY, useClass: UserMysqlRepository },
   ],
-  exports: [{ provide: USER_REPOSITORY_KEY, useClass: UserMysqlRepository }],
+  exports: [
+    { provide: USER_REPOSITORY_KEY, useClass: UserMysqlRepository },
+    UserQueryService,
+  ],
 })
 export class UserModule {}
