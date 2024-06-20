db2ef021fa731ae9ccc2064fda491329ddc2e0b0
Mipmipp <sebastianhernandoh@gmail.com>
2024-06-20 16:02:16 -0300

    Add create user validations

--- src/modules/iam/authentication/interface/dto/sign-up.dto.ts
+++ src/modules/iam/authentication/interface/dto/sign-up.dto.ts
@@ -1,12 +1,34 @@
-import { IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class SignUpDto {
   @IsString()
+  @MinLength(8)
+  @MaxLength(20)
   username: string;
 
-  @IsString()
+  @IsEmail()
   email: string;
 
   @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'password must contain at least one uppercase letter',
+  })
+  @Matches(/(?=.*[a-z])/, {
+    message: 'password must contain at least one lowercase letter',
+  })
+  @Matches(/(?=.*[0-9])/, {
+    message: 'password must contain at least one number',
+  })
+  @Matches(/(?=.*[!@#$%^&*])/, {
+    message: 'password must contain at least one special character',
+  })
   password: string;
 }
